38f0aca9f51fb736f30c65b179d5eade3ca6f754
Fernando <frr4212@gmail.com>
2024-12-10 16:36:47 -0400

    seccion de tesotimonios agregada

--- src/components/sections/section5.tsx
+++ src/components/sections/section5.tsx
@@ -5,8 +5,60 @@ function Section5() {
 
     return (
 
-        <div className="section5">
-            seccion5
+        <div className = "section5" >
+            
+            <div className = 'rejilla'>
+                <div className = "section5 text">
+                        <div className='testimonio-pic'> 
+                            <img className='testimonio-pic pic' src="src\imagenes\person-circle.png" alt="" />
+                        </div>
+                    <div>
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Iure quis error consequatur, dicta dignissimos debitis?
+                    </div>
+                </div>
+                <div className = "section5 text">
+                    <div className='testimonio-pic'>
+                        <img className='testimonio-pic pic' src="src\imagenes\person-circle.png" alt="" />
+                    </div>
+                    <div>
+                        Lorem ipsum dolor sit adipisicing elit. Iure quis error consequatur, dicta dignissimos debitis?
+                    </div>
+                </div>
+                <div className = "section5 text">
+                    <div className='testimonio-pic'>
+                        <img className='testimonio-pic pic' src="src\imagenes\person-circle.png" alt="" />
+                    </div>
+                    <div>
+                        Lorem ipsum dolor sit adipisicing elit. Iure quis error consequatur, dicta dignissimos debitis?
+                    </div>
+                </div>
+                <div className = "section5 text">
+                    <div className='testimonio-pic'>
+                        <img className='testimonio-pic pic' src="src\imagenes\person-circle.png" alt="" />
+                    </div>
+                    <div>
+                        Lorem ipsum dolor sit adipisicing elit. Iure quis error consequatur, dicta dignissimos debitis?
+                    </div>
+                </div>
+                <div className = "section5 text">
+                    <div className='testimonio-pic'>
+                        <img className='testimonio-pic pic' src="src\imagenes\person-circle.png" alt="" />
+                    </div>
+                    <div>
+                        Lorem ipsum dolor sit adipisicing elit. Iure quis error consequatur, dicta dignissimos debitis?
+                    </div>
+                </div>
+                <div className = "section5 text">
+                    <div className='testimonio-pic'>
+                        <img className='testimonio-pic pic' src="src\imagenes\person-circle.png" alt="" />
+                    </div>
+                    <div>
+                        Lorem ipsum dolor sit adipisicing elit. Iure quis error consequatur, dicta dignissimos debitis?
+                    </div>
+                </div>
+            </div>
+            
+            
         </div>
     )
 }
