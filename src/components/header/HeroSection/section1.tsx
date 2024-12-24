fd84ff19d19663ba48e6d70431b715c01d58167c
Dave Canache <106845716+dvcanache@users.noreply.github.com>
2024-12-23 23:49:02 -0400

    Navbar actualizada, hero section actualizada

--- src/components/header/HeroSection/section1.tsx
+++ src/components/header/HeroSection/section1.tsx
@@ -1,25 +1,47 @@
-import '../../sections/section.css'
-
+import "../../sections/section.css";
 
 function Section1() {
+  return (
+    <>
+      <div className=" absolute w-full h-full flex items-center justify-center p-4">
+        <div className="flex flex-row gap-[50px] ">
+          <div className="w-[450px] flex-1 bg-black bg-opacity-75 backdrop-filter backdrop-blur-lg text-white p-6 rounded-[60px]">
+            <h1 className="text-[70px] font-bold">Everest Gym</h1>
+            <p className="text-[30px] font-bold">
+              Lideres deportivos en el estado
+            </p>
+            <p>
+              Un cuerpo sano es una mente sana, en EverestGym te ayudamos a
+              alcanzar tus metas
+            </p>
+            <button style={{ marginLeft: "30%", marginTop: "10px" }}>
+              Unirse Ahora
+            </button>
+          </div>
 
-    return (
-
-        <div className="section1">
-            <div className="section1 textcontainer">
-                <h1 className='section1-title'>Everest Gym</h1>
-                <p className='section1-subtitle'>Lideres deportivos en el estado</p>
-                <p>Un cuerpo sano es una mente sana, en EverestGym te ayudamos a alcanzar tus metas</p>
-                <button style={{marginLeft: '30%', marginTop: '10px'}}>Unirse Ahora</button>
-                
-                
-            </div>
-            <div className="section1 imgcontainer">
-               <img className='section1 img' src="src\imagenes\ejercicioilustrado.png" alt="" />
-               
-            </div>
-            <img style={{position: 'absolute',bottom: '27%',left: '20%',height: '15%', width: '50%',zIndex: 999, marginLeft: '0',}} src="src\imagenes\flecha.png" alt="" />
+          <div className="w-[450px] h-[700px] flex-1 p-0 bg-black bg-opacity-25 backdrop-filter backdrop-blur-lg text-white  p-6 rounded-[60px]">
+            <img
+              className="object-cover w-full h-full overflow-visible"
+              src="src\imagenes\ejercicioilustrado.png"
+              alt=""
+            />
+          </div>
         </div>
-    )
+        <img
+          style={{
+            position: "absolute",
+            bottom: "27%",
+            left: "20%",
+            height: "15%",
+            width: "50%",
+            zIndex: 1,
+            marginLeft: "0",
+          }}
+          src="src\imagenes\flecha.png"
+          alt=""
+        />
+      </div>
+    </>
+  );
 }
-export default Section1
+export default Section1;
