4da44ea0f41518b0b1911cb1a9628e1c9a77d874
Andres <andresnsilvag@gmail.com>
2024-12-25 00:11:30 -0400

    footer, testimonios a tailwind

--- src/components/fondo/fondo.tsx
+++ src/components/fondo/fondo.tsx
@@ -1,8 +1,8 @@
 
 import './fondo.css'
 import Footer from '../footer/footer'
-import Section1 from '../sections/section1'
-import Section2 from '../sections/section2'
+import Section1 from '../header/HeroSection/section1'
+import Section2 from '../sections/section2/section2'
 import Section3 from '../sections/section3'
 import Section4 from '../sections/section4'
 import Section5 from '../sections/section5'
