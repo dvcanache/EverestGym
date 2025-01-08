c60aecd9bf1c58e79199b48636571daee6fb1455
Fernando <frr4212@gmail.com>
2025-01-07 23:14:49 -0400

    seccion 3 terminada
    responsive a las tarjetas agregado mejorado el estilo de las tarjetas
    encabezado de la seccion estilado

--- src/components/sections/section3/section3.tsx
+++ src/components/sections/section3/section3.tsx
@@ -4,15 +4,15 @@ function Section3 () {
     <div className="items-center flex flex-col border-none w-[90%] lg:w-[85%] h-auto lg:h-[80%] p-[40px] lg:p-[50px] bg-transparent text-white text-[20px] lg:text-[40px] rounded-[25px]">
 
       {/* Primera columna */}
-      <div className="flex flex-col w-full lg:w-[70%] h-auto lg:px-[30px] mb-[50px] lg:mb-0">
+      <div className="p-[30px] backdrop-blur-sm border-[1px] rounded-[40px] flex flex-col w-full lg:w-[70%] h-auto lg:px-[30px] mb-[50px] lg:mb-0">
         <div className="flex flex-col items-center">
-          <p className="font-bebas font-bold h-auto text-[16px] lg:text-[40px] w-full lg:w-[80%] mt-[10px] text-center">
+          <p className="font-bebas font-bold h-auto text-[26px] lg:text-[40px] w-full lg:w-[80%] text-center">
             Elige el plan que se adapte a tus metas y estilo de vida.
           </p>
         </div>
         <div>
-          <p className="font-oswald font-normal lg:mb-[20px] h-auto text-[16px] lg:text-[24px] w-full lg:w-full mt-[10px] text-center">
-            En Everest Gym la transformacion es accesible para todos. Inscripcion gratis en todos nuestros planes
+          <p className="font-oswald font-normal lg:mb-[0px] h-auto text-[16px] lg:text-[24px] w-full lg:w-full mt-[10px] text-center">
+            En Everest Gym la transformacion es accesible para todos. Inscripcion <span className="text-amarillo">gratis</span> en todos nuestros planes
           </p>
         </div>
       </div>
@@ -21,7 +21,7 @@ function Section3 () {
       <div className="overflow-hidden flex flex-row justify-between w-full lg:w-[50%]lg:pr-[50px] mb-[50px] lg:mb-0 h-auto">
         <PricingSlider />
       </div>
-
+      
     </div>
   );
 };
